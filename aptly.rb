46b1b26ff3f3236e90c8f357ce75ef5e770b69e7
Ryan Uber <ru@ryanuber.com>
2014-05-06 14:24:39 -0700

    Add test for stale lock (pid not running)

--- aptly.rb
+++ aptly.rb
@@ -21,12 +21,25 @@ module Aptly
     @@mutex_path = '/tmp/aptly.lock'
 
     def self.lock!
-      sleep 1 until !self.locked?
-      File.open(@@mutex_path, 'w'){}
+      while self.locked?
+        self.unlock! if !self.running?
+        sleep 5
+      end
+      File.open(@@mutex_path, 'w') {|f| f.write Process.pid}
     end
 
     def self.locked?
       File.exist? @@mutex_path
+    end
+
+    def self.running?
+      begin
+        pid = File.open(@@mutex_path).read.to_i
+        Process.kill(0, pid)
+        return true
+      rescue Errno::ESRCH, Errno::ENOENT
+        return false
+      end
     end
 
     def self.unlock!
