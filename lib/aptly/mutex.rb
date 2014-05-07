baa1636d7b674d4a098c65a1f21e19e23e32b52b
Ryan Uber <ru@ryanuber.com>
2014-05-06 18:01:02 -0700

    Documented mutex class

--- lib/aptly/mutex.rb
+++ lib/aptly/mutex.rb
@@ -4,6 +4,15 @@ module Aptly
   class Mutex
     @@mutex_path = '/tmp/aptly.lock'
 
+    # Attempts to acquire the aptly mutex. This method will wait if the mutex
+    # is already locked elsewhere, and check back every 5 seconds to see if it
+    # has been freed. On each check where the mutex is determined to be in use,
+    # we check if the process which originally acquired the lock is still
+    # running so we can acquire a stale mutex should we need to.
+    #
+    # == Returns:
+    # True, once the lock is acquired.
+    #
     def self.lock!
       while self.locked?
         self.unlock! if !self.running?
@@ -12,10 +21,21 @@ module Aptly
       File.open(@@mutex_path, 'w') {|f| f.write Process.pid}
     end
 
+    # Checks if the mutex is in use. This is done via a simple file check.
+    #
+    # == Returns:
+    # True if locked, else false
+    #
     def self.locked?
       File.exist? @@mutex_path
     end
 
+    # Checks if the process which originally acquired the mutex is still
+    # running. This is useful to determine staleness of a locked mutex.
+    #
+    # == Returns:
+    # True if running, else false
+    #
     def self.running?
       begin
         pid = File.open(@@mutex_path).read.to_i
@@ -26,6 +46,11 @@ module Aptly
       end
     end
 
+    # Unlocks the mutex so that other processes may acquire it.
+    #
+    # == Returns:
+    # True if unlock happens, else false
+    #
     def self.unlock!
       File.delete @@mutex_path if self.locked?
     end
