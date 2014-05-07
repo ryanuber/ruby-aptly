e18dda669728a1a2d15e11b0729f480c0488d6d0
Ryan Uber <ru@ryanuber.com>
2014-05-06 22:09:29 -0700

    Added yard docs to repo.rb

--- lib/aptly/repo.rb
+++ lib/aptly/repo.rb
@@ -1,6 +1,21 @@
 module Aptly
   extend self
 
+  # Creates a new repository in aptly
+  #
+  # == Parameters:
+  # name::
+  #   The name to use for the new repository
+  # dist::
+  #   The distribution used during publishing
+  # comment::
+  #   A comment describing the repository
+  # component::
+  #   The component used during publishing
+  #
+  # == Returns:
+  # An Aptly::Repo object
+  #
   def create_repo(
     name,
     dist: '',
@@ -20,11 +35,25 @@ module Aptly
     return Repo.new name
   end
 
+  # Return a list of existing repositories
+  #
+  # == Returns
+  # An array of strings representing repository names
+  #
   def list_repos
     out = runcmd 'aptly repo list'
     parse_list out.lines
   end
 
+  # Retrieve information about a repository
+  #
+  # == Parameters:
+  # name::
+  #   The name of the repository to retrieve information for
+  #
+  # == Returns:
+  # A hash of repository information
+  #
   def repo_info name
     out = runcmd "aptly repo show #{name.to_safe}"
     parse_info out.lines
@@ -40,6 +69,15 @@ module Aptly
     @comment = ''
     @num_packages = 0
 
+    # Instantiates an Aptly::Repo object
+    #
+    # == Parameters:
+    # name::
+    #   The name of the repository
+    #
+    # == Returns:
+    # An Aptly::Repo object
+    #
     def initialize name
       if !Aptly::list_repos.include? name
         raise AptlyError.new "Repo '#{name}' does not exist"
@@ -53,10 +91,19 @@ module Aptly
       @num_packages = info['Number of packages'].to_i
     end
 
+    # Drops an existing aptly repository
     def drop
       Aptly::runcmd "aptly repo drop #{@name.to_safe}"
     end
 
+    # Add debian packages to a repo
+    #
+    # == Parameters:
+    # path::
+    #   The path to the file or directory source
+    # remove_files::
+    #   When true, deletes source after import
+    #
     def add path, remove_files: false
       cmd = 'aptly repo add'
       cmd += ' -remove-files' if remove_files
@@ -64,6 +111,16 @@ module Aptly
       Aptly::runcmd cmd
     end
 
+    # Imports package resources from existing mirrors
+    #
+    # == Parameters:
+    # from_mirror::
+    #   The name of the mirror to import from
+    # pkg_spec::
+    #   A debian pkg_spec string (e.g. "libc6 (>= 2.7-1)")
+    # deps::
+    #   When true, follows package dependencies and adds them
+    #
     def import from_mirror, pkg_spec, deps: false
       cmd = 'aptly repo import'
       cmd += ' -with-deps' if deps
@@ -71,6 +128,18 @@ module Aptly
       Aptly::runcmd cmd
     end
 
+    # Copy package resources from one repository to another
+    #
+    # == Parameters:
+    # from_repo::
+    #   The source repository name
+    # to_repo::
+    #   The destination repository name
+    # pkg_spec::
+    #   A debian pkg_spec string
+    # deps::
+    #   When true, follow deps and copy them
+    #
     def copy from_repo, to_repo, pkg_spec, deps: false
       cmd = 'aptly repo copy'
       cmd += ' -with-deps' if deps
@@ -78,14 +147,28 @@ module Aptly
       Aptly::runcmd cmd
     end
 
+    # Shortcut method to copy resources in from another repository
     def copy_from from_repo, pkg_spec, deps: false
       copy from_repo, @name, pkg_spec, deps: deps
     end
 
+    # Shortcut method to copy resources out to another repository
     def copy_to to_repo, pkg_spec, deps: false
       copy @name, to_repo, pkg_spec, deps: deps
     end
 
+    # Move package resources from one repository to another
+    #
+    # == Parameters:
+    # from_repo::
+    #   The source repository name
+    # to_repo::
+    #   The destination repository name
+    # pkg_spec::
+    #   A debian pkg_spec string
+    # deps::
+    #   When true, follow deps and move them too
+    #
     def move from_repo, to_repo, pkg_spec, deps: false
       cmd = 'aptly repo move'
       cmd += ' -with-deps' if deps
@@ -93,18 +176,29 @@ module Aptly
       Aptly::runcmd cmd
     end
 
+    # Shortcut method to move packages in from another repo
     def move_from from_repo, pkg_spec, deps: false
       move from_repo, @name, pkg_spec, deps: deps
     end
 
+    # Shortcut method to move packages out to another repository
     def move_to to_repo, pkg_spec, deps: false
       move @name, to_repo, pkg_spec, deps: deps
     end
 
+    # Remove packages selectively from a repository
+    #
+    # == Parameters:
+    # pkg_spec::
+    #   A debian pkg_spec string to select packages by
+    #
     def remove pkg_spec
       Aptly::runcmd "aptly repo remove #{@name.to_safe} #{pkg_spec.to_safe}"
     end
 
+    # save allows you to modify the repository distribution, comment, or
+    # component string by using the attr_accessor's, and then calling this
+    # method to persist them to aptly.
     def save
       cmd = "aptly repo edit"
       cmd += " -distribution=#{@dist.to_safe}"
