848d9909840004e4a43715e4c16fefc9fea82ade
Ryan Uber <ru@ryanuber.com>
2014-05-08 09:15:11 -0700

    Updated snapshot script

--- scripts/snapshot.rb
+++ scripts/snapshot.rb
@@ -7,24 +7,33 @@ require 'aptly'
 existing = Aptly.list_snapshots
 while id = Time.now.to_i.to_s
   break if !existing.include? id
+  sleep 1
 end
 
 begin
-  # Create an array for tracking snapshots we create
+  to_snapshot = Array.new
   snapshots = Array.new
 
-  # Loop over mirrors
+  # First, update the mirrors
   Aptly.list_mirrors.each do |mirror_name|
-    snapshot_name = "#{mirror_name}.#{id}"
-
-    # Get mirror object and call a mirror update
     puts "==> Updating mirror: #{mirror_name}"
     mirror = Aptly::Mirror.new mirror_name
     mirror.update
+    to_snapshot << mirror
+  end
 
-    # Create a new snapshot of the mirror after the update.
-    puts "==> Creating mirror snapshot: #{snapshot_name}"
-    Aptly.create_mirror_snapshot snapshot_name, mirror_name
+  # Add repos to the snapshot work
+  Aptly.list_repos.each do |repo_name|
+    to_snapshot << Aptly::Repo.new(repo_name)
+  end
+
+  # Loop over collected resources and snapshot them
+  to_snapshot.each do |r|
+    snapshot_name = "#{r.name}.#{id}"
+
+    # Create a new snapshot of the resource
+    puts "==> Creating snapshot: #{snapshot_name}"
+    r.snapshot snapshot_name
 
     # Add it to our list of snapshots.
     snapshots << snapshot_name
@@ -37,6 +46,6 @@ begin
   puts "Done!"
   exit 0
 rescue AptlyError => e
-  Syslog.open("aptly_daily_snapshot") {|log| log.err e.message}
+  Syslog.open("aptly") {|log| log.err e.message}
   exit 1
 end
