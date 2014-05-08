f0ef2c10592475bac5bb1017e5d7e61616a12503
Ryan Uber <ru@ryanuber.com>
2014-05-08 16:54:27 -0700

    Added debian packaging stuff

---
+++ ext/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+NAME=ruby-aptly
+VER=0.1.0
+
+mkdir -p $NAME-$VER
+cp -R lib $NAME-$VER
+cp -R ext/debian $NAME-$VER
+tar czf "${NAME}_${VER}.orig.tar.gz" $NAME-$VER
+(cd $NAME-$VER; dpkg-source -b .)
+#pbuilder --build *.dsc
