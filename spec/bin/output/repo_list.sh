b4fa6ffc549bdff198b5d012021902010dfec86f
Ryan Uber <ru@ryanuber.com>
2014-05-10 12:20:32 -0700

    Use execution rather than flat text files so we can control stdout, stderr, and exit code easily.

--- spec/bin/output/repo_list.sh
+++ spec/bin/output/repo_list.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+cat > /dev/stdout <<EOF
 List of local repos:
  * [repo1] (packages: 90)
  * [repo2] (packages: 231)
 
 To get more information about local repository, run \`aptly repo show <name>\`.
+EOF
+exit 0
