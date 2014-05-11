6e617d923983ce3bb86dc97d75607e50f42c973e
Ryan Uber <ru@ryanuber.com>
2014-05-10 18:19:18 -0700

    Added tests for dropping repos

--- spec/aptly/repo_spec.rb
+++ spec/aptly/repo_spec.rb
@@ -21,5 +21,21 @@ module Aptly
       repo = Aptly::Repo.new 'repo1'
       repo.kind_of?(Aptly::Repo).should eq(true)
     end
+
+    it "should fail if the repo doesn't exist" do
+      expect { Aptly::Repo.new 'nothing' }.to raise_error
+    end
+  end
+
+  describe "Dropping Aptly Repos" do
+    it "should successfully drop a repo" do
+      repo = Aptly::Repo.new 'repo1'
+      repo.drop
+    end
+
+    it "should reflect the correct repos after dropping" do
+      repos = Aptly.list_repos
+      repos.should eq(['repo2'])
+    end
   end
 end
