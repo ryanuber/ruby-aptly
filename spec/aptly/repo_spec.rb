7a12d065d2db62b92348f41215bf34eb71ffbd83
Ryan Uber <ru@ryanuber.com>
2014-05-10 17:56:27 -0700

    Execute real_aptly with custom config to keep tests local

--- spec/aptly/repo_spec.rb
+++ spec/aptly/repo_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 module Aptly
   describe "Aptly Repos" do
+    it "should create repos successfully" do
+      Aptly.create_repo 'repo1'
+      Aptly.create_repo 'repo2'
+    end
+
     it "should return a list of repositories" do
       Aptly.list_repos.should eq(['repo1', 'repo2'])
     end
--- spec/setup.sh
+++ spec/setup.sh
@@ -2,4 +2,4 @@
 URL="http://dl.bintray.com/smira/aptly/0.5/debian-squeeze-x64/aptly"
 
 # Fetch aptly
-(mkdir -p bin && curl -OL $URL -o bin/real_aptly && chmod +x bin/real_aptly)
+(mkdir -p bin && curl -L -o bin/real_aptly $URL && chmod +x bin/real_aptly)
