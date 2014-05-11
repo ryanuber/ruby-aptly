b91923fd6cd5c80044e667f16ee7ec440618191a
Ryan Uber <ru@ryanuber.com>
2014-05-10 18:07:08 -0700

    Add clean task, add negative test for repo create

--- Rakefile
+++ Rakefile
@@ -3,8 +3,12 @@ require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new
 
-task :default => [:prep, :spec]
-
+task :default => [:test]
+task :test => [:prep, :spec, :clean]
 task :prep do
   %x(cd spec; sh setup.sh)
+end
+task :clean do
+  FileUtils.rm_rf '.aptly'
+  FileUtils.rm_rf 'spec/bin/real_aptly'
 end
--- spec/aptly/repo_spec.rb
+++ spec/aptly/repo_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module Aptly
-  describe "Aptly Repos" do
+  describe "Creating Aptly Repos" do
     it "should create repos successfully" do
       Aptly.create_repo 'repo1'
       Aptly.create_repo 'repo2'
@@ -9,6 +9,10 @@ module Aptly
 
     it "should return a list of repositories" do
       Aptly.list_repos.should eq(['repo1', 'repo2'])
+    end
+
+    it "should error if repo already exists" do
+      expect { Aptly.create_repo 'repo1' }.to raise_error
     end
   end
 end
